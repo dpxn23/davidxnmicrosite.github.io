153ca28a1f02a2a6a08cb12b89d9eeef0e529d4b
dpxn23 <davidxuan.nguyen01@gmail.com>
2021-02-01 16:27:47 -0500

    first-commit
    testing AJAX feature

---
+++ script.js
@@ -0,0 +1,18 @@
+function start(){
+    //$("body").css("background-color", "yellow");
+}
+
+$(window).on("load", start);
+
+function loadpage(e){
+
+    e.preventDefault();
+    $("#menu a.active").removeClass("active");
+    $(this).addClass("active");
+
+    $("#content").load("about html");
+
+}
+
+$(document).on("click", "#menu a", loadpage);
+
